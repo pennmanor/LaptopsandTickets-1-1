b502a6357bfe14093c39f8143bf1d8169145e9db
chuck-aka-ben <benjamin@thomasnetwork.net>
2013-08-22 13:58:35 -0400

    added manager files

--- index.php
+++ index.php
@@ -15,8 +15,9 @@ $tickets = Ticket::getAllByProperty(PROPERTY_STUDENT, $session->getID());
 	<title>1:1</title>
 	<link href="css/bootstrap.css" rel="stylesheet">
 	<link href="css/style.css" rel="stylesheet">
+	<link href="css/manager.css" rel="stylesheet">
+	
 </head>
-
 	<body>
 		<div class="navbar navbar-static-top">
 			<div class="navbar-inner">
@@ -59,35 +60,83 @@ $tickets = Ticket::getAllByProperty(PROPERTY_STUDENT, $session->getID());
 			else
 			{
 			?>
-				<table class="table table-bordered">
-					<thead>
-						<tr>
-							<th>Title</th>
-							<th>Date</th>
-							<th></th>
-						</tr>
-					</thead>
-			
-					<tbody>
-						<?php
-						foreach ($tickets as $ticket)
-						{
-							$properties = $ticket->getProperties();
-						?>
+			<div class="manager large" data-manager="tickets">
+				<div class="navbar navbar-static-top">
+					<div class="navbar-inner">
+						<div class="pull-right">
+							<button class="btn btn-inverse" data-manager-control="View">View</button>
+						</div>
+						
+					</div>
+				</div>
+				<div class="manager-results">
+					<table class="table">
+						<thead>
 							<tr>
-								<td><?php echo $properties[PROPERTY_TITLE]."&nbsp;&nbsp;".$ticket->getStateLabel(); ?> </td>
-								<td><?php echo date("M d, Y", $properties[PROPERTY_TIMESTAMP])." at ".date("g:i A", $properties[PROPERTY_TIMESTAMP]); ?></td>
-								<td><button class="btn btn-inverse pull-right" onClick="window.location = 'viewTicket.php?id=<?php echo $properties[PROPERTY_ID]; ?>'">View</button></td>
+								
+								<th>Title</th>
+								<th>Date</th>
+								<th><input type="checkbox"></th>
 							</tr>
-						<?php
-						}
-						?>
-					</tbody>
-				</table>
+						</thead>
+						<tbody>
+							<?php
+							foreach ($tickets as $ticket)
+							{
+								$properties = $ticket->getProperties();
+							?>
+								<tr>
+									
+									<td><?php echo $properties[PROPERTY_TITLE]."&nbsp;&nbsp;".$ticket->getStateLabel(); ?> </td>
+									<td><?php echo date("M d, Y", $properties[PROPERTY_TIMESTAMP])." at ".date("g:i A", $properties[PROPERTY_TIMESTAMP]); ?></td>
+									<td><input type="checkbox"></td>
+								</tr>
+							<?php
+							}
+							?>
+						</tbody>
+					</table>
+				</div>
+			</div>
 			<?php
 			}
 			?>
 		</div>
+		<script src="js/jquery.js" type="text/javascript"></script>
+		<script src="js/manager.js" type="text/javascript"></script>
+		<script>
+			var tickets = JSON.parse(<?php 
+			$ticketList = Array();
+			foreach($tickets as $ticket){
+				$properties = $ticket->getProperties();
+				array_push($ticketList, $properties[PROPERTY_ID]);
+			}
+			echo "\"".addslashes(json_encode($ticketList)). "\"";
+			 ?>);
+			$(document).ready(function(){
+				$.fn.manager.defaults.selectedClass = "info";
+				$("[data-manager-control=\"View\"]").hide()
+				$("[data-manager=\"tickets\"]").manager({
+					onSingleSelect : function(s){
+						var row = s[0];
+						$("[data-manager-control=\"View\"]").click(function(){
+							window.location = "viewTicket.php?id=" + tickets[row];
+						});
+						$("[data-manager-control=\"View\"]").show()
+					}, onNoneSelect : function(){
+						$("[data-manager-control=\"View\"]").hide()
+						$("[data-manager-control=\"View\"]").unbind("click");
+					}, onMulitipleSelect : function(){
+						$("[data-manager-control=\"View\"]").hide()
+						$("[data-manager-control=\"View\"]").unbind("click");
+					}
+				
+				});
+			});
+			
+			
+			
+		</script>
 	</body>
 
 </html>
