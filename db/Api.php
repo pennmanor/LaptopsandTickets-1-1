cae1a815f60ba8d3d68eab07751ed68ef5bac583
chuck-aka-ben <benjamin@thomasnetwork.net>
2013-09-10 09:00:12 -0400

    removed debug

--- db/Api.php
+++ db/Api.php
@@ -9,7 +9,7 @@ class Api{
 		if(!is_a($mysql, "mysqli")){
 			throw new Exception("API Object Failed to construct because no \$mymsql");
 		}
-		echo $query = "SELECT `id`, `key`, `name` FROM `apikeys` WHERE `key` = \"".$mysql->real_escape_string($key)."\"";
+		$query = "SELECT `id`, `key`, `name` FROM `apikeys` WHERE `key` = \"".$mysql->real_escape_string($key)."\"";
 		$result = $mysql->query($query);
 		if(!$result || mysqli_affected_rows($mysql) == 0) {
 			//throw new Exception("Invalid key");
