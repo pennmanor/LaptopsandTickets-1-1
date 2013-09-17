d8ffe60714f01932c366a064ad9595668ff906ab
Andrew <andrew@lobos.me>
2013-09-17 10:17:23 -0400

    Reworded interface

--- index.php
+++ index.php
@@ -76,12 +76,14 @@ if ( array_key_exists("logout", $_GET) )
 				echo "</ul>";
 			}
 			else{
-				echo "<p> No one is currently at the Student Help Desk. Why don't you try <a href=\"newTicket.php\">submitting a ticket</a>?</p>";
+				echo "<p> No one is currently at the Student Help Desk. You are welcome to <a href=\"newTicket.php\">submit a ticket</a>.</p>";
 			}
 			?>
 			<a href="newTicket.php" class="btn btn-large btn-primary">Submit a Ticket</a>
 			<?php if ( $showFeedbackForm ) { ?><a href="feedbackForm.php" class="btn btn-large btn-primary">Send Feedback</a><?php } ?>
 			<a href="http://blogs.pennmanor.net/1to1/" class="btn btn-large btn-primary">Read the Blog</a>
+			<br><br>
+			<p>Remember to login with your Penn Manor Google Docs user name and password</p>
 		</div>
 		<!-- <hr>
 		<div class="row-fluid">
