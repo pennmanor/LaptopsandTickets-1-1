2e3e74365b0316292b508d2014c891a1284ab57a
Benjamin Thomas <benjamin@thomasnetwork.net>
2014-04-23 13:39:17 -0400

    Fixed broken link

--- admin/students/student.php
+++ admin/students/student.php
@@ -49,7 +49,7 @@ if ( array_key_exists("sid", $_GET) )
 					<ul class="nav">
 						<li><a href="../index.php">Overview</a></li>
 						<li><a href="../tickets">Tickets</a></li>
-						<li><a href="./index.php">Laptops</a></li>
+						<li><a href="../laptops">Laptops</a></li>
 						<li class="active"><a href="../students">Students</a></li>
 						<li><a href="../calendar">Logs</a></li>
 						<?php if ( $showFeedbackForm ) { ?><li><a href="../feedback">Feedback</a></li><?php } ?>
